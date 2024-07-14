4aa8870eeee09e6b6b3b38cf7f6ecd87d0b9baca
wfxey <158351052+wfxey@users.noreply.github.com>
2024-07-15 00:58:28 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,23 +2,24 @@ from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
-    
+
 setup(
     name='diec',
     version='0.1',
     packages=find_packages(),
     license='MIT',
-    description='A tool that encodes text and give out a key, that you can decode with this program too!',
+    description='A tool that encodes text and provides a key for decoding!',
     author='D&I',
     author_email='projectsdi02@gmail.com',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/wfxey/binaryconvert',
     download_url='https://github.com/D-I-Projects/diec/archive/refs/tags/v0.1.tar.gz',
-    keywords=['binary', '8-bit', 'text-to-binary'],
+    keywords=['diec', 'encoding', 'decoding'],
     install_requires=[
-        #Empty
+        # Specify dependencies here, if any
     ],
+    python_requires='>=3.9',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
