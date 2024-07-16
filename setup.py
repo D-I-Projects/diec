7924c5358a222050709af01bf206a61ea2b16666
wfxey <158351052+wfxey@users.noreply.github.com>
2024-07-16 15:46:50 +0200

    Update setup.py
    Signed-off-by: wfxey <158351052+wfxey@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='diec',
-    version='1.5',
+    version='1.6',
     packages=find_packages(),
     license='MIT',
     description='A tool that encodes text and provides a key for decoding!',
@@ -14,7 +14,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/wfxey/diec',
-    download_url='https://github.com/D-I-Projects/diec/archive/refs/tags/v1.5.tar.gz',
+    download_url='https://github.com/D-I-Projects/diec/archive/refs/tags/v1.6.tar.gz',
     keywords=['diec', 'encoding', 'decoding'],
     install_requires=[
         'binaryconvert',
